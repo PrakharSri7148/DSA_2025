1d33687b72c3a8903d61025a55ab0daa8861f73b
Prakhar Srivastava <prakharsri2021@gmail.com>
2025-10-12 15:58:40 +0530

    Stack java collection framework

---
+++ StackQuestions/StackJcf.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class StackJcf {
+    public static void main(String[] args) {
+        Stack <Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()) {
+            System.out.println(s.peek());
+            int val = s.pop();
+            System.out.println(val);
+        }
+    }
+}
