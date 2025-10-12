417b413f33746aeaecf263af9c7505915dbf9379
Prakhar Srivastava <prakharsri2021@gmail.com>
2025-10-12 20:39:43 +0530

    Linked List Collection framework

---
+++ LinkedListQuestions/LinkedListCfj.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class LinkedListCfj {
+    
+    public static void main(String[] args) {
+        LinkedList<Integer> lll = new LinkedList<>();
+
+        lll.addFirst(1);
+        lll.addFirst(2);
+        lll.addFirst(3);
+
+        System.out.println(lll);
+    }
+}
