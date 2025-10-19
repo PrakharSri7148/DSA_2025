5881feee38329189ba255a789ffc95a88fe4e98e
Prakhar Srivastava <prakharsri2021@gmail.com>
2025-10-19 17:23:17 +0530

    Update QueuesJcf.java

--- QueuesQuestions/QueuesJcf.java
+++ QueuesQuestions/QueuesJcf.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class QueuesJcf {
     public static void main(String[] args) {
-            Queue<Integer> q = new LinkedList<>();
+            Queue<Integer> q = new LinkedList<>();   //ArrayDeque is also a way to implement 
             q.add(1);
             q.add(2);
             q.add(3);
