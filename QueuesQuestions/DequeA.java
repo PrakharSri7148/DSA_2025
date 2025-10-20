5c92d5c844a05892f4be9de4466fe1a8e85e596d
Prakhar Srivastava <prakharsri2021@gmail.com>
2025-10-20 15:53:50 +0530

    Duque operations
    Operations:-
    addFirst();
    addLast();
    removeLast();
    removeFirst();
    getFirst();
    getLast();

---
+++ QueuesQuestions/DequeA.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class DequeA {
+    public static void main(String[] args) {
+        Deque<Integer> d = new LinkedList<>();
+        d.addFirst(1);
+        d.addLast(2);
+        d.addFirst(0);
+        d.addLast(3);
+        System.out.println(d);
+        System.out.println(d.getFirst());
+        System.out.println(d.getLast());
+        d.removeFirst();
+        d.removeLast();
+        System.out.println(d);
+    }
+
+}
+
