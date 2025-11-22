8f579aaec45efa48987dbbd92a0d8df4a345dc45
Prakhar Srivastava <prakharsri2021@gmail.com>
2025-11-22 14:44:42 +0530

    Subarray Sum Equals K
    Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
    
    A subarray is a contiguous non-empty sequence of elements within an array.
    
    Leetcode: 560

---
+++ ArraysQuestions/SubarraySum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class SubarraySum {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        int k = 3;
+
+        // HashMap<Arrays, Integer> map = new HashMap<>();
+
+        int count =0;
+
+        for(int i=0; i<nums.length; i++) {
+            int sum=0;
+            for(int j=i; j<nums.length; j++) {
+                sum = sum+nums[j];
+
+                if(sum == k) count++;
+            }
+        }
+
+        System.out.println(count);
+
+
+    }   
+}
