edd59d743187f017c9a22f13c038fde14021db18
Prakhar Srivastava <prakharsri2021@gmail.com>
2026-01-26 15:55:17 +0530

    Bellman Ford algorithm for Shortest Path
    Implementing Bellman Ford algorithm for finding shortest path from the source

---
+++ Graphs/BellmanFord.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class BellmanFord {
+    
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    public static void bellmanFord(ArrayList<Edge>[] graph, int src) {
+        int[] dist = new int[graph.length];
+        for(int i=0; i<graph.length; i++) {
+            if( i != src ) dist[i] = Integer.MAX_VALUE;
+        }
+
+        int V = graph.length;
+        for(int i=0; i< V-1; i++) {        //we have to perform this step V-1 Times 
+            for(int j=0; j<graph.length; j++) {
+                for(int k=0; k<graph[j].size(); k++) {
+
+                    Edge e = graph[j].get(k);
+
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    if(dist[u] + wt < dist[v]) dist[v] = dist[u] + wt;
+
+                }
+            }
+        }
+
+        for(int i : dist) {
+            System.out.print(i+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        int v = 6;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+
+        for(int i=0; i<graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add( new Edge(0, 1, 2));
+        graph[0].add( new Edge(0, 2, 4));
+
+        graph[1].add( new Edge(1, 2, -4));
+
+        graph[2].add( new Edge(2, 3, 2));
+
+        graph[3].add( new Edge(3, 4, 4));
+
+        graph[4].add( new Edge(4, 1, -1));
+
+        int src = 0;
+
+        bellmanFord(graph, src);
+        
+    }
+}
