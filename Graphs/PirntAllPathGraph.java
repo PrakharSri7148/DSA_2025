6dcc2a8aa2895a4de9d4c389ce0f3736cc58effc
Prakhar Srivastava <prakharsri2021@gmail.com>
2026-01-26 10:13:45 +0530

    Print all paths from Source to Target
    Time Complexity: O(V^v) worst case scenario 
    Not a preferred approach

---
+++ Graphs/PirntAllPathGraph.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class PirntAllPathGraph {
+
+    static class Edge{
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void printPath(ArrayList<Edge>[] graph, int src, int dest, String path) {
+        if(src == dest) {
+            System.out.println(path + dest);
+            return;
+        }
+
+        for(int i=0; i<graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+
+            printPath(graph, e.dest, dest, path+src);
+        }
+    }
+    public static void main(String[] args) {
+        
+        int v = 6;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+
+        for(int i=0; i<graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add( new Edge(0, 3));
+
+        graph[2].add( new Edge(2, 3));
+
+        graph[3].add( new Edge(3, 1));
+
+        graph[4].add( new Edge(4, 0));
+        graph[4].add( new Edge(4, 1));
+
+        graph[5].add( new Edge(5, 0));
+        graph[5].add( new Edge(5, 2));
+
+        int src = 5;
+        int dest = 1;
+
+        printPath(graph, src, dest, "");
+        
+    }
+}
